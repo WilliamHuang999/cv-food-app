733c509949debd8c5239dabae35d0c8d41b579b4
William <68077292+WilliamHuang999@users.noreply.github.com>
2023-06-29 22:49:23 -0400

    start of classification of Fruits 360 dataset

---
+++ buildModel.py
@@ -0,0 +1,63 @@
+import tensorflow as tf
+import os
+from keras.utils import img_to_array, load_img
+import numpy as np
+import cv2 as cv
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import EarlyStopping
+import matplotlib.pyplot as plt
+
+trainPath = r"C:\Users\gener\OneDrive\Documents\cv-food-app\archive\fruits-360\Training"
+testPath = r"C:"
+
+batchSize = 64      # Reduce value if you have less GPU
+
+# Read in example image and get shape
+img = load_img(trainPath + r"\Quince\0_100.jpg")
+plt.imshow(img)
+plt.show()
+
+imgA = img_to_array(img)
+print(imgA.shape)
+
+# Build model
+model = Sequential()
+model.add(Conv2D(filters=128, kernel_size=3, activation="relu", input_shape=(100,100,3)))
+model.add(MaxPooling2D())
+model.add(Conv2D(filters=64, kernel_size=3, activation="relu"))
+model.add(Conv2D(filters=32, kernel_size=3, activation="relu"))
+model.add(MaxPooling2D())
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(5000, activation="relu"))
+model.add(Dense(1000, activation="relu"))
+model.add(Dense(131, activation="softmax"))
+
+print(model.summary())
+
+# Compile model
+model.compile(loss="categorical_crossentropy", optimizer="SGD", metrics=["accuracy"])
+
+# Load data
+train_datagen = ImageDataGenerator(rescale=1./255, shear_range=0.3, horizontal_flip=True, vertical_flip=True, zoom_range=0.3)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(trainPath, target_size=(100,100), batch_size=batchSize, color_mode="rgb", class_mode="categorical", shuffle=True)
+
+
+test_generator = test_datagen.flow_from_directory(trainPath, target_size=(100,100), batch_size=batchSize, color_mode="rgb", class_mode="categorical")
+
+stepsPerEpoch = np.ceil(train_generator.samples / batchSize)
+validationSteps = np.ceil(test_generator.samples / batchSize)
+
+# Early stopping
+stop_early = EarlyStopping(monitor="val_accuracy", patience=5)      # Stop fitting model if it doesn't improve by 5
+
+history = model.fit(train_generator, steps_per_epoch=stepsPerEpoch, epochs=50, validation_data=test_generator, validation_steps=validationSteps, callbacks=[stop_early])
+
+model.save("=================")     # Add file path hear to save the model to
+
+
---
+++ testModel.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import os
+from keras.utils import img_to_array, load_img
+import numpy as np
+import cv2 as cv
+
+# Load model
+model = tf.keras.models.load_model("===========")       # Put in model path here
+print(model.summary())
+
+# Load categories
+source_folder = "Test folder here"
+categories = os.listsdir(source_folder).sort()
+print(categories)
+print(len(categories))      # Should be 131
+
+# Load and prepare image
+def prepareImage(path):
+    img = load_img(img, target_size=(100,100))
+    imgArray = img_to_array(img)
+    print(imgArray.shape)
+    imgArray = np.expand_dims(imgArray, axis=0)
+    imgArray = imgArray / 255.
+    return imgArray
+
